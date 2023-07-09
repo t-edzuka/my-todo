3f243a9d34368e9ebf4076c49b412b1ba808299b
edzuka <t.edzuka@happy-quality.jp>
2023-07-09 11:34:03 +0900

    :white_check_mark: test & refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use axum::{routing::get, Router};
+use axum::response::IntoResponse;
+use axum::Json;
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use hyper::StatusCode;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::net::SocketAddr;
 
@@ -6,19 +13,104 @@ async fn root() -> &'static str {
     "Hello, world!"
 }
 
-#[tokio::main]
-async fn main() {
-    // init logging
+fn setup_logging() {
     let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
+}
 
-    let app = Router::new().route("/", get(root));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8078));
+fn create_app() -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user))
+}
 
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
+async fn run_server(socket_addr: &SocketAddr, app: Router) {
+    tracing::debug!("listening on {}", socket_addr);
+    axum::Server::bind(socket_addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    // init logging
+    setup_logging();
+
+    let app = create_app();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8078));
+
+    run_server(&addr, app).await;
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User::new(1, payload.name);
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+struct CreateUser {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+struct User {
+    id: u64,
+    name: String,
+}
+
+impl User {
+    fn new(id: u64, name: String) -> Self {
+        Self { id, name }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, header::CONTENT_TYPE, Method}, 
+    };
+    
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn test_root() {
+        let req = Request::builder()
+            .uri("/")
+            .method(Method::GET)
+            .body(Body::empty())
+            .unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body = std::str::from_utf8(&bytes).unwrap();
+        assert_eq!(body, "Hello, world!");
+    }
+
+    #[tokio::test]
+    async fn test_create_user() {
+        // Create reauest for user.
+        let req = Request::builder()
+            .header(CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+            .method(Method::POST)
+            .uri("/users")
+            .body(Body::from(r#"{ "name" : "田中 太郎"}"#))
+            .unwrap();
+
+        let res = create_app().oneshot(req).await.unwrap();
+
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body = std::str::from_utf8(&bytes).unwrap();
+        let user = serde_json::from_str::<User>(body).expect("Cannot convert to User struct.");
+        assert_eq!(
+            user,
+            User {
+                id: 1,
+                name: "田中 太郎".to_string()
+            }
+        );
+    }
 }
